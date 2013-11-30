8a6508471e42b899f0d0549a1483b5a387312edb
Lais <lminchillo2@gmail.com>
2013-11-30 10:53:54 -0200

    Updated main activity layout

--- gen/lais/lightsout/R.java
+++ gen/lais/lightsout/R.java
@@ -32,6 +32,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int new_game_easy=0x7f050001;
+        public static final int new_game_hard=0x7f050003;
+        public static final int new_game_medium=0x7f050002;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
