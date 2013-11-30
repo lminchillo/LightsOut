09b4186c85b817268661444254f9365296bc5757
Lais <lminchillo2@gmail.com>
2013-11-30 13:46:07 -0200

    Updated game activity

--- src/lais/lightsout/GameEasyActivity.java
+++ src/lais/lightsout/GameEasyActivity.java
@@ -1,82 +1,110 @@
 package lais.lightsout;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ImageButton;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.TextView;
-import android.widget.AdapterView;
+import android.widget.ImageView;
 
 public class GameEasyActivity extends Activity
 {
-	
-	GridView gridView;
-	
-	static final String[] numbers = new String[] { 
-		"1", "1", "0",
-		"0", "1", "0",
-		"0", "0", "1"};
-	 
-	/*static final bool[][] numbers = new bool[][]{
-	{false, false, true},
-	{true, true, true},
-	{false, true, false} 
-	};*/
-	
-	/*static final String[] numbers = new String[] { 
-			"A", "B", "C", "D", "E",
-			"F", "G", "H", "I", "J",
-			"K", "L", "M", "N", "O",
-			"P", "Q", "R", "S", "T",
-			"U", "V", "W", "X", "Y", "Z"};*/
+	private GridView gridView;
+	private boolean[][] lights = new boolean[5][5];
  
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_easy);
 		
 		initialize();
 	}
-
-	private boolean light = false;
 	
 	private void initialize()
 	{
-		gridView = (GridView) findViewById(R.id.gridView1);
-		 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, numbers);
- 
-		gridView.setAdapter(adapter);
- 
-		gridView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v,
-				int position, long id) {
-			   Toast.makeText(getApplicationContext(),
-				((TextView) v).getText(), Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		final ImageButton button = (ImageButton) findViewById(R.id.game_easy_button);
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v)
+		for (int i=0; i<5; i++)
+		{
+			for (int j=0; j<5; j++)
 			{
-				if (light)
+				lights[i][j] = Math.random()>0.5;
+			}
+		}
+		
+		gridView = (GridView) findViewById(R.id.gridView1);
+		
+		LightAdapter adapter = new LightAdapter(getApplicationContext(), 0);
+		
+		gridView.setAdapter(adapter);
+		
+		gridView.setOnItemClickListener(new OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			{
+				Log.d("Game","onItemClick");
+				
+				int j = position%5, i = (position-j)/5;
+				
+				ImageView b = (ImageView) view;
+				
+				if (lights[i][j])
 				{
-					button.setImageDrawable(getResources().getDrawable(R.drawable.button_off));
+					b.setImageDrawable(getResources().getDrawable(R.drawable.button_off));
 				}
 				else
 				{
-					button.setImageDrawable(getResources().getDrawable(R.drawable.button_on));
+					b.setImageDrawable(getResources().getDrawable(R.drawable.button_on));
 				}
-				light = !light;
+				lights[i][j] = !lights[i][j];
 			}
 		});
+	}
+	
+	class LightAdapter extends ArrayAdapter<Boolean>
+	{
+		Context ctx;
+		
+		public LightAdapter(Context context, int resource)
+		{
+			super(context, resource);
+			ctx = context;
+		}
+		
+		@Override
+		public int getCount()
+		{
+			return 25;
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			if (convertView == null)
+			{
+				convertView = new ImageView(ctx);
+				//convertView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+			}
+			ImageView img = (ImageView) convertView;
+			
+			int j = position%5, i = (position-j)/5;
+			if (lights[i][j])
+			{
+				img.setImageDrawable(getResources().getDrawable(R.drawable.button_on));
+			}
+			else
+			{
+				img.setImageDrawable(getResources().getDrawable(R.drawable.button_off));
+			}
+			
+			return convertView;
+		}
 	}
 }
