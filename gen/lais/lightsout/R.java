4e57bb92356a29b81fb2d34936bc22310fd79e8b
Lais <lminchillo2@gmail.com>
2013-11-30 10:46:51 -0200

    Added drawables and created main activity layout

--- gen/lais/lightsout/R.java
+++ gen/lais/lightsout/R.java
@@ -17,10 +17,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int button_beige=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int button_beige=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
+        public static final int main_easy=0x7f070001;
+        public static final int main_hard=0x7f070003;
+        public static final int main_medium=0x7f070002;
         public static final int main_title=0x7f070000;
     }
     public static final class layout {
--- src/lais/lightsout/MainActivity.java
+++ src/lais/lightsout/MainActivity.java
@@ -1,7 +1,10 @@
 package lais.lightsout;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -10,5 +13,23 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initialize();
+	}
+	
+	private void initialize()
+	{
+		TextView title = (TextView) findViewById(R.id.main_title);
+		title.setTypeface(Typeface.createFromAsset(getAssets(), "Existence-Light.ttf"),Typeface.BOLD);
+		
+		Button easy = (Button) findViewById(R.id.main_easy);
+		easy.setTypeface(Typeface.createFromAsset(getAssets(), "Existence-Light.ttf"),Typeface.BOLD);
+		
+		Button medium = (Button) findViewById(R.id.main_medium);
+		medium.setTypeface(Typeface.createFromAsset(getAssets(), "Existence-Light.ttf"),Typeface.BOLD);
+		
+		Button hard = (Button) findViewById(R.id.main_hard);
+		hard.setTypeface(Typeface.createFromAsset(getAssets(), "Existence-Light.ttf"),Typeface.BOLD);
+		
 	}
 }
